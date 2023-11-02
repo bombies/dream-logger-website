b34f85f4b4e150de7d94e40e8a575109666b62c7
bombies <xjamesdzn@gmail.com>
2023-11-02 10:38:53 -0500

    Update privacy policy

--- src/app/(site)/(external)/privacy/page.tsx
+++ src/app/(site)/(external)/privacy/page.tsx
@@ -7,7 +7,7 @@ const PrivacyPolicy: FC = () => {
         <main className="flex flex-col items-center py-32 phone:py-16">
             <h1 className="font-bold text-6xl phone:text-3xl mb-6">Privacy Policy</h1>
             <article className="w-1/2 tablet:w-2/3 phone:w-5/6 bg-secondary/40 rounded-xl p-12 phone:px-6">
-                <h6 className="text-sm text-subtext mb-3">Last Updated: October 26, 2023</h6>
+                <h6 className="text-sm text-subtext mb-3">Last Updated: November 2, 2023</h6>
                 <p>
                     {
                         "DreamLogger (\"we\", \"our\", or \"us\") is committed to protecting the privacy of our users (\"you\" or \"your\"). This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you access or use our website or mobile application (the \"Service\"). By using the Service, you agree to the practices described in this Privacy Policy."
@@ -43,6 +43,22 @@ const PrivacyPolicy: FC = () => {
                         <p>{"Account Deletion: You have the right to delete your account at any time. You can do this instantly by accessing the account settings within the Service."}</p>
                     </div>
                     <div>
+                        <h3 className="font-bold text-2xl">Cookies</h3>
+                        <Spacer y={6}/>
+                        <h4 className="font-semibold text-xl">User Authentication and Sessions</h4>
+                        <p>We use cookies to enhance your experience and ensure the security of your user sessions.
+                            These cookies are employed solely for the purpose of user authentication and session
+                            management, and they are essential for the proper functioning of the Service. They do not
+                            collect any personal information and are not used for any advertising or tracking purposes.
+                            Our use of cookies for these specific functions is in line with industry standards to
+                            maintain the security and integrity of your sessions. By using our Service, you agree to the
+                            use of these cookies for authentication and session management. You may disable cookies in
+                            your browser settings, but please be aware that this may affect your ability to use certain
+                            features of the Service. For more information about cookies, please refer to your
+                            browser&apos;s
+                            help documentation.</p>
+                    </div>
+                    <div>
                         <h3 className="font-bold text-2xl">Data Sharing</h3>
                         <p>{"We do not share your personal information with third parties for advertising purposes. However, please note that this policy is subject to change, and any future changes will be communicated to you through the Service."}</p>
                     </div>
@@ -63,7 +79,7 @@ const PrivacyPolicy: FC = () => {
                         <p>{"If you have any questions about this Privacy Policy or our data practices, please contact us at ajani.green@outlook.com."}</p>
                     </div>
                 </div>
-                <Divider className="my-6" />
+                <Divider className="my-6"/>
                 <p>{"By using the Service, you acknowledge that you have read, understood, and agree to be bound by this Privacy Policy."}</p>
             </article>
         </main>
