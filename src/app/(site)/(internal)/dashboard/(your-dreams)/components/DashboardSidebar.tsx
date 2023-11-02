2f5c6646be1a8a05a4e891959bf71615911fea64
bombies <xjamesdzn@gmail.com>
2023-11-02 17:47:25 -0500

    Add sidebar item for tags and characters

--- src/app/(site)/(internal)/dashboard/(your-dreams)/components/DashboardSidebar.tsx
+++ src/app/(site)/(internal)/dashboard/(your-dreams)/components/DashboardSidebar.tsx
@@ -8,28 +8,34 @@ import SidebarProvider from "@/app/(site)/components/sidebar/SidebarProvider";
 import CalendarIcon from "@/app/(site)/components/icons/CalendarIcon";
 import SearchFilledIcon from "@/app/(site)/components/icons/SearchFilledIcon";
 import StatisticsIcon from "@/app/(site)/components/icons/StatisticsIcon";
+import TagIcon from "@/app/(site)/components/icons/TagIcon";
 
 const DashboardSidebar: FC = () => {
     return (
         <SidebarProvider>
             <Sidebar headerText="Your Dashboard">
                 <SidebarItem
-                    startContent={<CloudIcon />}
+                    startContent={<CloudIcon/>}
                     title="Your Dreams"
                     href="/dashboard"
                 />
                 <SidebarItem
-                    startContent={<CalendarIcon />}
+                    startContent={<CalendarIcon/>}
                     title="Dream Calendar"
                     href="/dashboard/calendar"
                 />
                 <SidebarItem
-                    startContent={<SearchFilledIcon />}
+                    startContent={<TagIcon/>}
+                    title="Tags & Characters"
+                    href="/dashboard"
+                />
+                <SidebarItem
+                    startContent={<SearchFilledIcon/>}
                     title="Search Dreams"
                     href="/dashboard"
                 />
                 <SidebarItem
-                    startContent={<StatisticsIcon />}
+                    startContent={<StatisticsIcon/>}
                     title="Dream Reports"
                     href="/dashboard"
                 />
