caa9dd6485ae76257a16049c5d2b4c167ba8d51b
bombies <xjamesdzn@gmail.com>
2023-11-11 15:23:46 -0500

    Update dream containers

--- src/app/(site)/(internal)/dashboard/(your-dreams)/components/dreams/containers/DreamContainer.tsx
+++ src/app/(site)/(internal)/dashboard/(your-dreams)/components/dreams/containers/DreamContainer.tsx
@@ -1,5 +1,7 @@
 import {FC, PropsWithChildren} from "react";
 import clsx from "clsx";
+import Card from "@/app/(site)/components/Card";
+import {CardBody} from "@nextui-org/card";
 
 type Props = {
     title: string,
@@ -12,13 +14,12 @@ const DreamContainer: FC<Props> = ({id, title, children, className, containerCla
     return (
         <div id={id} className={clsx(className, "flex flex-col")}>
             <h2 className="text-4xl phone:text-2xl font-semibold mb-8 phone:mb-4 tablet:text-center">{title}</h2>
-            <div
-                className={clsx(
-                    containerClassName,
-                    "bg-light-secondary border border-primary dark:border-none dark:bg-secondary rounded-3xl py-6 px-4 phone:p-3 w-[36rem] tablet:w-[24rem] phone:w-[20rem] tablet:self-center flex flex-col gap-y-6 phone:gap-y-3"
-                )}>
-                {children}
-            </div>
+            <Card
+                className={clsx(className, "py-6 px-4 phone:p-3 w-[36rem] tablet:w-[24rem] phone:w-[20rem] tablet:self-center")}>
+                <CardBody className="flex flex-col gap-y-6 phone:gap-y-3 overflow-hidden">
+                    {children}
+                </CardBody>
+            </Card>
         </div>
     )
 }
